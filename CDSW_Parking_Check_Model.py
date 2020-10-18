62f47dc8b3070fb63d01860d69c7c2ca91484edd
indiaga <agabang@gmail.com>
2020-10-19 00:02:03 +0900

    2020-10-19

---
+++ CDSW_Parking_Check_Model.py
@@ -0,0 +1,152 @@
+import cdsw
+
+# data visualisation and manipulation
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline  
+
+#model selection
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.metrics import accuracy_score,precision_score,recall_score,confusion_matrix,roc_curve,roc_auc_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import LabelEncoder
+
+#preprocess.
+from keras.preprocessing.image import ImageDataGenerator
+
+#dl libraraies
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam,SGD,Adagrad,Adadelta,RMSprop
+from keras.utils import to_categorical
+
+# specifically for cnn
+from keras.layers import Dropout, Flatten,Activation
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+from keras.callbacks import ModelCheckpoint
+ 
+import tensorflow as tf
+import random as rn
+
+# specifically for manipulating zipped images and getting numpy arrays of pixel values of images.
+import cv2                  
+import numpy as np  
+from tqdm import tqdm
+import os                   
+from random import shuffle  
+from zipfile import ZipFile
+from PIL import Image
+
+X=[]
+Z=[]
+IMG_SIZE=150
+
+FREE_DIR='./image-data/free'
+FULL_DIR='./image-data/full'
+
+def assign_label(img,label):
+    return label
+  
+def make_train_data(label,DIR):
+    for img in tqdm(os.listdir(DIR)):
+        label=assign_label(img,label)
+        path = os.path.join(DIR,img)
+        img = cv2.imread(path,cv2.IMREAD_COLOR)
+        img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
+        
+        X.append(np.array(img))
+        Z.append(str(label))
+        
+        
+# make 'Free' data
+make_train_data('Free',FREE_DIR)
+print(len(X))
+
+# make 'Full' data
+make_train_data('Full',FULL_DIR)
+print(len(X))
+
+le=LabelEncoder()
+Y=le.fit_transform(Z)
+Y=to_categorical(Y,2)
+X=np.array(X)
+X=X/255
+
+# separate data
+x_train,x_test,y_train,y_test=train_test_split(X,Y,test_size=0.25,random_state=42)
+
+# fix random seed
+np.random.seed(42)
+rn.seed(42)
+tf.random.set_seed(42)
+
+# # modelling starts using a CNN.
+
+model = Sequential()
+model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same',activation ='relu', input_shape = (150,150,3)))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+
+model.add(Conv2D(filters = 64, kernel_size = (3,3),padding = 'Same',activation ='relu'))
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+ 
+
+model.add(Conv2D(filters =96, kernel_size = (3,3),padding = 'Same',activation ='relu'))
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+
+model.add(Conv2D(filters = 96, kernel_size = (3,3),padding = 'Same',activation ='relu'))
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dense(2, activation = "softmax"))
+
+batch_size=128
+epochs=50
+
+# data augmentation to prevent overfitting
+datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0.1, # Randomly zoom image 
+        width_shift_range=0.2,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.2,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+
+datagen.fit(x_train)
+model.compile(optimizer=Adam(lr=0.001),loss='categorical_crossentropy',metrics=['accuracy'])
+model.summary()
+
+# 모델학습: 가장 좋은 결과의 모델을 파일로 자동저장
+History = model.fit_generator(datagen.flow(x_train,y_train, batch_size=batch_size),
+                              epochs = epochs, validation_data = (x_test,y_test),
+                              verbose = 1, steps_per_epoch=x_train.shape[0] // batch_size,
+                              callbacks=[ModelCheckpoint('parking_check_model.h5', monitor='val_accuracy', save_best_only=True, verbose=1)]
+                              )
+
+plt.plot(History.history['loss'])
+plt.plot(History.history['val_loss'])
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epochs')
+plt.legend(['train', 'test'])
+plt.show()
+
+plt.plot(History.history['accuracy'])
+plt.plot(History.history['val_accuracy'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epochs')
+plt.legend(['train', 'test'])
+plt.show()
